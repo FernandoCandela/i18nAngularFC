33f54ce8d1093aef7ea2e5e5dcae471f4fd13887
Fernando Candela <20171937@aloe.ulima.edu.pe>
2022-10-08 11:10:34 -0500

    add environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: false,
+  apirestURL:'https://tidtou5lp9.execute-api.us-east-2.amazonaws.com/prod/',
+  ACCESS_TOKEN: 'idToken',
+  CLIENT_ID: '6cedka0sqhfhi2676um0c03gup',
+  AUTHORIZATION: 'Authorization',
+  BEARER: 'Bearer '
 };
